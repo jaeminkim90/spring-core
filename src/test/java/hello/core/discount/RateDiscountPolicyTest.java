500e5d85f1062ef3b4546ecc7777decd155959d4
Jaemin Kim <jaeminkim90@gmail.com>
2022-01-15 17:00:00 +0900

    TEST: VIP 10% 할인 정책 테스트 구현 완료

--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -1,14 +1,28 @@
 package hello.core.discount;
 
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class RateDiscountPolicyTest {.
+class RateDiscountPolicyTest {
 
-    RateDiscountPolicy
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Test
-    void discount() {
+    @DisplayName("VIP 10% 할인 적용 테스트")
+    void VIP_정률_고정_할인() {
+        // given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+
+        // when
+        int discountPrice = discountPolicy.discount(member, 10000);
+
+        // then
+        Assertions.assertThat(discountPrice).isEqualTo(1000);
+
     }
 }
