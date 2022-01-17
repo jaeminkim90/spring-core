dee4f55d1c92d6b95fe2860adda69e935cd4a25a
Jaemin Kim <jaeminkim90@gmail.com>
2022-01-17 16:34:39 +0900

    TEST: xml 방식으로 스프링 Bean을 등록하는 테스트 코드 구현 완료

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -26,7 +26,6 @@ public class OrderApp {
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-
         Order order = orderService.createOrder(memberId, "itemA", 20000);
         System.out.println("order: " + order);
         System.out.println("order.calculatePrice(): "+order.calculatePrice());
