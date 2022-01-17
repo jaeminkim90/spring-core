69d10a68d8f7af5d85e4b6f6c30c28f5fc5d25e3
Jaemin Kim <jaeminkim90@gmail.com>
2022-01-17 09:42:34 +0900

    TEST: Bean 이름으로 조회, 구체 타입으로 조회, 이름을 조회 실패 테스트 코드 구현 완료

--- src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -13,9 +13,9 @@ class ApplicationContextInfoTest {
     @Test
     @DisplayName("모든 빈 출력하기")
     void findAllBean() {
-        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames(); // 스프링에 등록된 모든 빈 이름 조회
         for (String beanDefinitionName : beanDefinitionNames) {
-            Object bean = ac.getBean(beanDefinitionName);// object가 꺼내짐으로, getBean을 이용하여 Bean을 꺼낸다
+            Object bean = ac.getBean(beanDefinitionName);// getBean(): Bean 이름으로 Bean 객체(인스턴스)를 조회
             System.out.println("name = " + beanDefinitionName + " object: " + bean);
         }
     }
@@ -25,7 +25,8 @@ class ApplicationContextInfoTest {
     void findApplicationBean() {
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
-            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName); // Bean 메타데이터
+            // getBeanDefinition(): Bean 이름으로 Bean의 메타데이터 조회
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
 
             // ROLE.APPLICATION: 개발자가 애플리케이션 개발을 위해 등록한 bean 또는 외부 라이브러리
             // ROLE.INFRASTRUCTURE: 스프링 내부에서 사용하는 bean
@@ -35,4 +36,6 @@ class ApplicationContextInfoTest {
             }
         }
     }
+
+
 }
