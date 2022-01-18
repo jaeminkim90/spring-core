6b02f9db5624c35207132a55df04ad9f8fe408c4
Jaemin Kim <jaeminkim90@gmail.com>
2022-01-18 21:26:10 +0900

    TEST: 무상태(stateless)가 아닐 경우, 싱글톤 패턴이 상태를 공유하여 발생하는 문제 테스트 코드 구현 완료

---
+++ src/test/java/hello/core/singletonTest/StatefulServiceTest.java
@@ -0,0 +1,43 @@
+package hello.core.singletonTest;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+     ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        // ThreadA: A 사용자 10000원 주문
+        statefulService1.order("userA", 10000);
+
+        // ThreadB: B 사용자 20000원 주문
+        statefulService1.order("userA", 20000);
+
+        // ThreadA: 사용자 A 주문 금액 조회
+        int price = statefulService1.getPrice();
+
+        System.out.println("price = " + price); // 20000원이 나온다. B 사용자로 인해 상태값 변경
+
+        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+
+        @Bean
+        public StatefulService statefulService () {
+            return new StatefulService();
+        }
+
+    }
+
+}
