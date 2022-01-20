314d27b93ec0adfe23a856e6db3c3634d1f1a1c1
Jaemin Kim <jaeminkim90@gmail.com>
2022-01-20 20:33:31 +0900

    FEAT: 중복된 Bean이 2개 이상 조회될 때 @Primary를 이용하여 우선순위 지정

--- src/main/java/hello/core/discount/FixDiscountPoliocy.java
+++ src/main/java/hello/core/discount/FixDiscountPoliocy.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("fixDiscountPolicy")
 public class FixDiscountPoliocy implements DiscountPolicy {
 
     private int discountFixAmount = 1000; // 1000월 고정 할인 금액
