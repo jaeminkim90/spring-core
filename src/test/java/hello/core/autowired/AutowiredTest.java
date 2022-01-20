1db181106f8aa35995951e2b2383d9200e38f983
Jaemin Kim <jaeminkim90@gmail.com>
2022-01-20 13:52:59 +0900

    TEST: @Autowired 옵션 처리 시, 결과를 확인할 수 있는 테스트 코드 구현 완료

---
+++ src/test/java/hello/core/autowired/AutowiredTest.java
@@ -0,0 +1,39 @@
+package hello.core.autowired;
+
+import hello.core.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.lang.Nullable;
+
+import java.util.Optional;
+
+public class AutowiredTest {
+
+    @Test
+    void AutowiredOption() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+    }
+
+    static class TestBean {
+
+        // 결과: 의존 관계가 없으면 호출하지 않는다.
+        @Autowired(required = false)
+        public void setNoBean1(Member noBean1) {
+            System.out.println("noBean1 = " + noBean1);
+        }
+
+        // 결과: 호출은 되지만, Bean에 등록된 객체가 없으므로, null 값을 가진다.
+        @Autowired
+        public void setNoBean2(@Nullable Member noBean2) {
+            System.out.println("noBean2 = " + noBean2);
+        }
+
+        // 결과: Bean에 등록된 객체가 없으면, Optional.empty를 가진다. 값이 있으면 Optional에 감싸져있다.
+        @Autowired
+        public void setNoBean3(Optional<Member> noBean3) {
+            System.out.println("noBean3 = " + noBean3);
+        }
+    }
+}
